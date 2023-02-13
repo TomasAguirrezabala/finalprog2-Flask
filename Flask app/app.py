5204642624c491249d004a891374c017d04dfafe
Tomas Aguirrezabala <tomiaguirrezabala@gmail.com>
2023-02-13 14:24:54 -0300

    w y r jsons

--- Flask app/app.py
+++ Flask app/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask import jsonify
+import json
+
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "santiago cabezon y re cabezon"
+    return "hola"
+
+peliculas = [{'nombre':'el senior de los anillos', 'director':'tomas', 'anio':1999, 'id':1} , {'nombre':'batman', 'director':'tomas', 'anio':'2013','id':2}]
+# esto crea los jsons
+with open('peliculas.json', 'w') as datosPeliculas:
+    json.dump( peliculas, datosPeliculas)
+    print(datosPeliculas)
+
+
+# esto carga los jsons
+@app.route("/pelis")
+def pelis():
+    with open('peliculas.json', 'r') as basePeliculas:
+        peli = json.load(basePeliculas)
+        print('funciono')
+        return peli    
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
