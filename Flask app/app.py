33c0084e96e2c7df15c2e027c24f6934ae2c4616
Tomas Aguirrezabala <tomiaguirrezabala@gmail.com>
2023-02-13 21:24:12 -0300

    ..

--- Flask app/app.py
+++ Flask app/app.py
@@ -6,27 +6,39 @@ import requests as rq
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hola"
+# @app.route('/')
+# def index():
+#     return "hola"
 
-peliculas = [{'nombre':'el senior de los anillos', 'director':'tomas', 'anio':1999, 'id':1} , {'nombre':'batman', 'director':'tomas', 'anio':'2013','id':2}]
-# esto crea los jsons
-with open('peliculas.json', 'w') as datosPeliculas:
-    json.dump( peliculas, datosPeliculas)
-    print(datosPeliculas)
+# peliculas = [{'nombre':'el senior de los anillos', 'director':'tomas', 'anio':1999, 'id':1} , {'nombre':'batman', 'director':'tomas', 'anio':'2013','id':2}]
+# # esto crea los jsons
+# with open('peliculas.json', 'w') as datosPeliculas:
+#     json.dump( peliculas, datosPeliculas)
+#     print(datosPeliculas)
 
 
 # esto carga los jsons
-@app.route("/pelis")
-def pelis():
-    with open('peliculas.json', 'r') as basePeliculas:
-        peli = json.load(basePeliculas)
-        print('funciono')
-        return peli   
-    
+# @app.route("/pelis")
+# def pelis():
+#     with open('peliculas.json', 'r') as basePeliculas:
+#         peli = json.load(basePeliculas)
+#         print('funciono')
+#         return peli   
 
-    
+# with open('peliculas.json', 'r') as datosPeliculas:
+#     peliculas = json.load(datosPeliculas)
+
+# with open('generos.json', 'r') as datosGeneros:
+#     generos = json.load(datosGeneros)
+
+
+@app.route("/generos")
+def getGeneros():
+    with open('generos.json', 'r') as datosGeneros:
+        generos = json.load(datosGeneros)
+    return jsonify(generos)
+        
+        
 if __name__ == '__main__':
     app.run(debug=True)
     
